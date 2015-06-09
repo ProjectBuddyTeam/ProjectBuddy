199a172442e26f8acc1d3a5b44241b0035e5d45e
Donny Reynolds <donnywith@gmail.com>
2015-06-09 00:33:57 +0000

    improves the look of project singles

--- app/models/project/project.rb
+++ app/models/project/project.rb
@@ -1,4 +1,6 @@
 class Project::Project < ActiveRecord::Base
   belongs_to :member, class_name: 'Identity::Member', foreign_key: 'identity_member_id'
   acts_as_taggable_on :tags
+
+  delegate :username, to: :member, prefix: true
 end
