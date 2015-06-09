825ee398c08935ee6c76d328e42b3c72ab430185
Donny Reynolds <donnywith@gmail.com>
2015-06-09 14:38:42 -0700

    designs project show

--- app/models/project/project.rb
+++ app/models/project/project.rb
@@ -2,5 +2,5 @@ class Project::Project < ActiveRecord::Base
   belongs_to :member, class_name: 'Identity::Member', foreign_key: 'identity_member_id'
   acts_as_taggable_on :tags
 
-  delegate :username, to: :member, prefix: true
+  delegate :username, :created_at, to: :member, prefix: true
 end
