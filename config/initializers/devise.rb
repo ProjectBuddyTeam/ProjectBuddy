3b429d154ed1a2ed27d37364d07e878654f65b0b
Donny Reynolds <donnywith@gmail.com>
2015-06-14 03:54:47 +0000

    adds production mail server settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'iamrobot@projectbuddy.io'
+  config.mailer_sender = 'projectbuddybot@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
