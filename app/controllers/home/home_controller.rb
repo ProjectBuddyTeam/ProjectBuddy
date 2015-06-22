43285db98144c329dd027b0bb7e89fb081d4e9c2
Donny Reynolds <donnywith@gmail.com>
2015-06-22 04:10:13 +0000

    orders homepage projects by time created

--- app/controllers/home/home_controller.rb
+++ app/controllers/home/home_controller.rb
@@ -1,5 +1,5 @@
 class Home::HomeController < ApplicationController
   def show
-    @projects = Project::Project.all.active
+    @projects = Project::Project.all.active.order(created_at: :desc)
   end
 end
